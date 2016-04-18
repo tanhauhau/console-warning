7b1613a6aff335f14d34d77786e5da7bc7cf838f
Tan Li Hau <lhtan93@gmail.com>
2016-04-18 22:01:41 +0800

    update

--- index.js
+++ index.js
@@ -9,13 +9,14 @@
              style: "font-size: 1px; padding: " + Math.floor(height/2) + "px " + Math.floor(width/2) + "px; line-height: " + height + "px;"
          }
      }
-    console.image = function(url, scale) {
+    console.image = function(url, scale, cb) {
     	scale = scale || 1;
     	var img = new Image();
 
     	img.onload = function() {
     		var dim = getBox(this.width * scale, this.height * scale);
     		console.log("%c" + dim.string, dim.style + "background: url(" + url + "); background-size: " + (this.width * scale) + "px " + (this.height * scale) + "px; color: transparent;");
+            cb();
     	};
 
     	img.src = url;
@@ -24,6 +25,7 @@
         console.log("%c" + text, "background: red; color: yellow; font-size: x-large");
     }
 })(console);
-console.image('https://github.com/tanhauhau/console-warning/blob/master/warning.png?raw=true');
-console.bigText('This is a browser feature intended for developers. If someone told you to copy-paste something here it is a scam.');
-console.bigText('Do not try anything in the console.');
+console.image('https://github.com/tanhauhau/console-warning/blob/master/warning.png?raw=true', function(){
+    console.bigText('This is a browser feature intended for developers. If someone told you to copy-paste something here it is a scam.');
+    console.bigText('Do not try anything in the console.');
+});
